c7a486fec989baa172a4deabde25d0fdb3ffc0f0
JulieChoJulie <h32cho@gmail.com>
2019-07-04 01:07:36 -0400

    use sessionmiddleware in socket

--- socket.js
+++ socket.js
@@ -1,11 +1,16 @@
 const SocketIO = require('socket.io');
+const axios = require('axios');
 
-module.exports = (server, app) => {
+module.exports = (server, app, sessionMiddleware) => {
     const io = SocketIO(server, { path: '/socket.io' });
 
     app.set('io', io);
     const room = io.of('/room');
     const chat = io.of('/chat');
+
+    io.use((socket, next) => {
+        sessionMiddleware(socket.request, socket.request.res, next);
+    });
 
     room.on('connection', (socket) => {
         console.log('connect to room name space');
@@ -18,13 +23,35 @@ module.exports = (server, app) => {
         console.log('connect to chat name space');
         const req = socket.request;
         const { headers: { referer } } = req;
+        // roomId =>URL = socket.request.headers.referer
         const roomId = referer
             .split('/')[referer.split('/').length -1]
             .replace(/\?.+/, '');
         socket.join(roomId);
+        socket.to(roomId).emit('join', {
+            user: 'system',
+            chat: `${req.session.color} is entered.`,
+        });
         socket.on('disconnect', () => {
             console.lg('disconnect with chat name space');
             socket.leave(roomId);
+            const currentRoom = socket.adapter.rooms[roomId];
+            const userCount = currentRoom ? currentRoom.length : 0;
+            if (userCount === 0) {
+                axios.delete(`http://localhost:8005/room/${roomId}`)
+                    .then(()=> {
+                        console.log('Successfully deleted the room');
+                    })
+                    .catch((error) => {
+                        console.error(error);
+                    });
+            } else {
+                socket.to(roomId).emit('exit', {
+                    user: 'system',
+                    chat: `${req.session.color} exits the room.`,
+                });
+            }
+
         });
     });
 
